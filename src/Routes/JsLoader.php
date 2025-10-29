7b0f4b8d3db21ac05dfa3f1ec066e18088fa0d72
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:33:45 +0100

    Version bump to 1.0.4

--- src/Routes/JsLoader.php
+++ src/Routes/JsLoader.php
@@ -1,20 +1,22 @@
 <?php
+
 namespace Tualo\Office\PKPass\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 
-class JsLoader implements IRoute{
-    public static function register(){
-        BasicRoute::add('/jspkpass/(?P<file>[\w.\/\-]+).js',function($matches){
+class JsLoader extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
+        BasicRoute::add('/jspkpass/(?P<file>[\w.\/\-]+).js', function ($matches) {
             App::contenttype('application/javascript');
-            if (file_exists(dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js')){
-                App::etagFile( dirname(__DIR__,1).'/js/lazy/'.$matches['file'].'.js', true);
+            if (file_exists(dirname(__DIR__, 1) . '/js/lazy/' . $matches['file'] . '.js')) {
+                App::etagFile(dirname(__DIR__, 1) . '/js/lazy/' . $matches['file'] . '.js', true);
                 BasicRoute::$finished = true;
                 http_response_code(200);
-            }            
-        },['get'],false);
-
+            }
+        }, ['get'], false);
     }
 }
--- src/Routes/PKPass.php
+++ src/Routes/PKPass.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tualo\Office\PKPass\Routes;
 
 use Tualo\Office\Basic\TualoApplication as App;
@@ -6,70 +7,71 @@ use Tualo\Office\Basic\Route as BasicRoute;
 use Tualo\Office\Basic\IRoute;
 use Tualo\Office\PKPass\PKPass as P;
 
-class PKPass implements IRoute{
-    public static function register(){
-        BasicRoute::add('/pkpass/test',function($matches){
-            try{
+class PKPass extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
+        BasicRoute::add('/pkpass/test', function ($matches) {
+            try {
                 App::contenttype('application/json');
 
                 $utc = new \DateTimeImmutable('2038-12-25 17:30:00', new \DateTimeZone('Europe/Berlin'));
                 $utc->setTimezone(new \DateTimeZone("UTC"));
 
-                $param=[];
-                
-                $param['KEY_ADDITIONAL_ACCESSCODE:LABEL']='Zugriffscode';
-                $param['KEY_ADDITIONAL_ACCESSCODE:VALUE']='123456';
-                
-                $param['KEY_PRIMERY:LABEL']='Ticket';
-                $param['KEY_PRIMERY:VALUE']='Musterveranstaltung';
+                $param = [];
+
+                $param['KEY_ADDITIONAL_ACCESSCODE:LABEL'] = 'Zugriffscode';
+                $param['KEY_ADDITIONAL_ACCESSCODE:VALUE'] = '123456';
+
+                $param['KEY_PRIMERY:LABEL'] = 'Ticket';
+                $param['KEY_PRIMERY:VALUE'] = 'Musterveranstaltung';
 
 
 
-                $param['KEY_LOCATION:LABEL']='Ort';
-                $param['KEY_LOCATION:VALUE']='Musterort';
-
-                
-                $param['KEY_BACK_PRICE:LABEL']='Kategorie';
-                $param['KEY_BACK_PRICE:VALUE']='Muster';
+                $param['KEY_LOCATION:LABEL'] = 'Ort';
+                $param['KEY_LOCATION:VALUE'] = 'Musterort';
 
 
-                $param['KEY_BACK_DATA_Additional_Front1:LABEL']='Details';
-                $param['KEY_BACK_DATA_Additional_Front1:VALUE']=''; // $event['description_md'];
+                $param['KEY_BACK_PRICE:LABEL'] = 'Kategorie';
+                $param['KEY_BACK_PRICE:VALUE'] = 'Muster';
 
-                $param['KEY_BACK_DATA_Additional_Front2:LABEL']='Einschränkungen';
-                $param['KEY_BACK_DATA_Additional_Front2:VALUE']='Zutritt ausschließlich ab 18 Jahre und nur mit gültigem Nachweis! Admission only from the age of 18 and only with valid proof!';
 
-                $param['KEY_BACK_DATA_Additional_Front3:LABEL']='Einschränkungen';
-                $param['KEY_BACK_DATA_Additional_Front3:VALUE']='Es gibt leider keinen Aufzug oder barrierefreien Zugang. Der Eingang und die Toiletten sind nur über Treppen zu erreichen.';
+                $param['KEY_BACK_DATA_Additional_Front1:LABEL'] = 'Details';
+                $param['KEY_BACK_DATA_Additional_Front1:VALUE'] = ''; // $event['description_md'];
 
-                $param['KEY_BACK_DATA_Additional_Front4:LABEL']='';
-                $param['KEY_BACK_DATA_Additional_Front4:VALUE']='';
+                $param['KEY_BACK_DATA_Additional_Front2:LABEL'] = 'Einschränkungen';
+                $param['KEY_BACK_DATA_Additional_Front2:VALUE'] = 'Zutritt ausschließlich ab 18 Jahre und nur mit gültigem Nachweis! Admission only from the age of 18 and only with valid proof!';
 
-                $param['KEY_BACK_DATA_Additional_Front5:LABEL']='';
-                $param['KEY_BACK_DATA_Additional_Front5:VALUE']='';
-                
+                $param['KEY_BACK_DATA_Additional_Front3:LABEL'] = 'Einschränkungen';
+                $param['KEY_BACK_DATA_Additional_Front3:VALUE'] = 'Es gibt leider keinen Aufzug oder barrierefreien Zugang. Der Eingang und die Toiletten sind nur über Treppen zu erreichen.';
+
+                $param['KEY_BACK_DATA_Additional_Front4:LABEL'] = '';
+                $param['KEY_BACK_DATA_Additional_Front4:VALUE'] = '';
+
+                $param['KEY_BACK_DATA_Additional_Front5:LABEL'] = '';
+                $param['KEY_BACK_DATA_Additional_Front5:VALUE'] = '';
+
                 // 2023-06-07T17:30+00:00
-                $param['utc_datetime']=str_replace(' ','T',$utc->format("Y-m-d H:i+00:00"))              ;
+                $param['utc_datetime'] = str_replace(' ', 'T', $utc->format("Y-m-d H:i+00:00"));
 
-                $param['KEY_SECONDARY_TICKETTYPENAME:LABEL']='Kategorie';
-                $param['KEY_SECONDARY_TICKETTYPENAME:VALUE']=$ticket['cat'];
-                
-                $param['KEY_HEADER_LOCATION_DATE:LABEL']='Am';
-                $param['KEY_HEADER_LOCATION_DATE:VALUE']=$utc->format("d.m.Y H:i");
-                
-                $param['KEY_SECONDARY_TICKETTYPENAME']=$ticket['cat'];
+                $param['KEY_SECONDARY_TICKETTYPENAME:LABEL'] = 'Kategorie';
+                $param['KEY_SECONDARY_TICKETTYPENAME:VALUE'] = $ticket['cat'];
 
-                $param['apple_logoText']='';
-                $param['apple_backgroundColor']='rgb(0,0,0)';
-                $param['apple_foregroundColor']='rgb(255,255,255)';
-                $param['apple_labelColor']='rgb(240,240,240)';
+                $param['KEY_HEADER_LOCATION_DATE:LABEL'] = 'Am';
+                $param['KEY_HEADER_LOCATION_DATE:VALUE'] = $utc->format("d.m.Y H:i");
 
-                P::pass('test',$param);
+                $param['KEY_SECONDARY_TICKETTYPENAME'] = $ticket['cat'];
+
+                $param['apple_logoText'] = '';
+                $param['apple_backgroundColor'] = 'rgb(0,0,0)';
+                $param['apple_foregroundColor'] = 'rgb(255,255,255)';
+                $param['apple_labelColor'] = 'rgb(240,240,240)';
+
+                P::pass('test', $param);
                 exit();
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 App::result('msg', $e->getMessage());
             }
-        },['get'],true);
-
+        }, ['get'], true);
     }
 }
